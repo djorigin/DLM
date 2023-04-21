9f1d73d213dca1066178824a0a62561fde4a3b29
darklightMeta <dc5spa@hotmail.com>
2023-04-21 23:30:40 +0930

    Register Client with admin.py

--- sourcecode/personnelManager/admin.py
+++ sourcecode/personnelManager/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('-date_joined',)
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password', 'email', 'first_name', 'middle_name', 'last_name')
+        }),
+        ('Personal Information', {
+            'fields': ('phone', 'gender', 'date_of_birth', 'age', 'profile_photo')
+        }),
+        ('Government Information', {
+            'fields': ('tax_file_number', 'licence_number')
+        }),
+        ('Permissions', {
+            'fields': ('is_staff', 'is_active')
+        }),
+    )
